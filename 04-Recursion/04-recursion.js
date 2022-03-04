3a44718d79a9dffce477905bb4d0157f385b40ac
Leandro <lbuitragoc04@gmail.com>
2022-03-03 22:29:59 -0500

    recursion done

--- 04-Recursion/04-recursion.js
+++ 04-Recursion/04-recursion.js
@@ -4,7 +4,14 @@
 // producto([7, 2, 5]) devuelve 70
 const producto = function (array) {
   //escribe aqui tu codigo
-  
+  let product = 0;
+  if (array.length === 1) {
+    product = array.shift()
+  }
+    while (array.length) {
+      product = array.shift() * producto(array)
+    }
+  return product;
 };
 
 // Dado un objeto con objetos anidados utilizar la recursión para crear una función
@@ -24,5 +31,25 @@ const producto = function (array) {
 // }
 const isThere = function (obj, value) {
   //escribe aqui tu codigo
-};
+  let aux = 0;
+  let test = Object.keys(obj)
+
+  if (typeof obj[test[0]] === 'object') {
+    aux = isThere(obj[test], value)
+  }else{
+    
+    for (let i = 0; i < test.length; i++){
+      if (obj[test[i]] == value) {
+        aux = true;
+        return aux}
+      } 
+      if (aux === 0) {
+        aux = false;
+        return false
+      }
+  } 
+  return aux
+}
+
+
 module.exports = { producto, isThere };
