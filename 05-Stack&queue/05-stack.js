784cd1fb4764a598505a372a9eb8608a02764eb7
Leandro <lbuitragoc04@gmail.com>
2022-03-07 11:46:44 -0500

    Stack done

--- 05-Stack&queue/05-stack.js
+++ 05-Stack&queue/05-stack.js
@@ -17,18 +17,69 @@ const { Stack, Queue } = require("../estructuras")
 
 function efectoEspejo(str){  
 //tu codigo aqui
+    let newArr = new Stack();
+    let aux = "";
+    let answer = "";
 
+        for (let i = 0; i < str.length; i++){
+            if (str[i] === " "){
+                newArr.push(aux);
+                aux = "";
+            } else {
+                aux = aux + str[i];
+            }
+        }
+
+    newArr.push(aux);
+    aux = "";    
+
+        while (newArr.size() !== 0){
+            if (newArr.size() === 1) {
+            aux = aux + newArr.array[newArr.size()-1];
+            newArr.pop();
+        } else {
+            aux = aux + newArr.array[newArr.size()-1] + " ";
+        }
+        newArr.pop();
+    }
+
+    for ( let i = 0; i< aux.length; i++) {
+        newArr.push(aux[i]) ;
+    }
+    
+    while (newArr.size()) {
+        answer = answer + newArr.pop();
+    }
+    
+    return answer;
 
 };
-
-
-
-
-
-
-
 
 module.exports = {
     efectoEspejo,
    
 }
+
+/*
+function efectoEspejo(str){
+
+const reverse = new Stack()
+
+let temp = '';
+
+for (let i = 0; i < str.length; i++) {
+    if(str[i] === ' ') {
+        while(reverse.size() > 0){
+           temp += reverse.pop(); 
+        }
+        temp += str[i]
+    }  else {
+        reverse.push(str[i])
+    }
+}
+
+while(reverse.size() > 0) temp += reverse.pop(); 
+return temp
+
+}
+*/
