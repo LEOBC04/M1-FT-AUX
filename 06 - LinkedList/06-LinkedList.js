a6dcca617740a3140a01f8ba9d7b19652ac2dc6e
Leandro <lbuitragoc04@gmail.com>
2022-03-08 09:39:45 -0500

    linkedlist done

--- 06 - LinkedList/06-LinkedList.js
+++ 06 - LinkedList/06-LinkedList.js
@@ -3,6 +3,12 @@ const { LinkedList, Node } = require("../estructuras")
   // Este metodo deberia retornar la cantidad de elementos de la lista
   LinkedList.prototype.size=function(){
       
+    var curr = this.head;
+    var count = this.len;
+
+    if ( curr === null) return 0;
+
+    return count;
   }
 
 
@@ -14,7 +20,20 @@ const { LinkedList, Node } = require("../estructuras")
 // ACLARACIÓN: Se debe ordenar la lista original y no una nueva.
 LinkedList.prototype.orderList = function () {
     // Tu código aca:
-    
+    let current = this.head;
+    let newArr = []
+    while (current) {
+      newArr.push(current.value);
+      current = current.next
+    }
+    newArr = newArr.sort((a,b)=>b-a);
+    current = this.head;
+
+    for (let i = 0; i < newArr.length; i++) {
+      current.value = newArr[i];
+      current = current.next
+    }
+ 
   };
 
 
@@ -24,9 +43,28 @@ LinkedList.prototype.orderList = function () {
   //tomar el head como posicion 0
    
   // 1-->2-->3-->2.5-->4   2.5 pos=1  newnodo= 2.5-->4   curr=3   
-LinkedList.prototype.insert = function(data,pos) {
+ LinkedList.prototype.insert = function(data,pos) {
+
+  if (this.len < pos) return this.add(data)
+  let newNode = new Node(data)
+
+    if (pos ==1) {
+      newNode.next = this.head
+      this.head = newNode
+    }
+
+    let current = this.head
+    while (pos>1) {
+      pos--;
+      current = current.next
+    }
+
+    newNode.next = current.next;
+    current.next = newNode;
+    this.len++
+  }
 	
-}
+
   
 
 
@@ -35,6 +73,28 @@ LinkedList.prototype.insert = function(data,pos) {
 module.exports={
   LinkedList
 }
+/*let counter = 1;
+        let current = this.head;
+        let aux = current.next;
+        var newNode = new Node(data)
+        //Si pos= 0
+    
+        if (pos === 0) {
+          this.head = newNode;
+          newNode.next = current;
+        }
 
+        //Si pos es diferente de cero 
+
+        while (current.next) {
+            if (counter === pos){
+                current.next = newNode;
+                newNode.next = aux;
+                return;
+            }
+            current =current.next;
+            aux= aux.next
+        }
+        this.len++ */
 
 
